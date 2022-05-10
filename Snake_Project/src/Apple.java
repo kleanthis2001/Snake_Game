43c00e619b11b6d3ec8475046c0e775187f5a3c5
kleanti bashalli <58070317+kleanthis2001@users.noreply.github.com>
2022-05-11 01:25:54 +0300

    Finished snake game

---
+++ Snake_Project/src/Apple.java
@@ -0,0 +1,16 @@
+
+public class Apple {
+SnakeGame main;
+public int posX;
+public int posY;
+
+public Apple(int startX, int startY) {
+posX = startX;
+posY = startY;
+}
+@SuppressWarnings("static-access")
+public void setRandomPosition() {
+posX = (int) (Math.random() * main.WIDTH);
+posY = (int) (Math.random() * main.HEIGHT);
+}
+}
